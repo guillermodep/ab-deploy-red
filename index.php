e929d48e34f09c541a533acdb514821f0434206f
Guille Dep <58941444+guillermodep@users.noreply.github.com>
2020-10-08 09:03:49 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "<center><h1>Prueba deploy en alta disponibilidad y deploy A - B</h1</center><br/><br/> \n";
+
+echo "Hola SRE, This is VERSION 1.0 ------   My Pod IP is : ";
+
+echo "<p> <font color=red>";
+
+echo file_get_contents('/etc/hostname');
+
+echo "<center><br/> \n";
+
+echo "<img src=https://github.com/guillermodep/http-base/blob/master/star_elefant.png?raw=true>";
+
+ 
+?>
